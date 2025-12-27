1fa8867847d9d7853738da231da0dd19b08e4b12
scriptkid777 <servsystem@yandex.ru>
2025-12-28 00:17:08 +0700

    4. TODO list [#3786 #556288] refactor #1

--- src/main/java/ru/job4j/todo/configuration/HibernateConfiguration.java
+++ src/main/java/ru/job4j/todo/configuration/HibernateConfiguration.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class HibernateConfiguration {
     @Bean(destroyMethod = "close")
