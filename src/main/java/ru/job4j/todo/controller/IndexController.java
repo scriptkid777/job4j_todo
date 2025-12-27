dceabe61c0c625a34fc805b39de31006b3398598
scriptkid777 <servsystem@yandex.ru>
2025-12-28 00:11:24 +0700

    4. TODO list [#3786 #556288] refactor #1

--- src/main/java/ru/job4j/todo/controller/IndexController.java
+++ src/main/java/ru/job4j/todo/controller/IndexController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class IndexController {
     @RequestMapping({"/", "/index"})
